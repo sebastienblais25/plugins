5640e45578a4927e2cf322466faad28b781889e1
Miles Petrov <miles.petrov@canada.ca>
2019-04-04 12:11:48 -0400

    chore(build): update npm publish

--- bin/build/travis/before_deploy.sh
+++ bin/build/travis/before_deploy.sh
@@ -3,6 +3,12 @@
 if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/plugins" ]; then
     openssl aes-256-cbc -K $encrypted_acf2fcf52a75_key -iv $encrypted_acf2fcf52a75_iv -in bin/build/cloud_rsa.enc -out /tmp/cloud_rsa -d
     chmod 600 /tmp/cloud_rsa
+
+    if [ ! "$TRAVIS_TAG" ]; then
+        npm version prerelease
+        CURRENT_VERSION=$(node -p "require('./package.json').version")
+        git push "https://$GITHUB_TOKEN@github.com/fgpv-vpgf/plugins.git" "v$CURRENT_VERSION" HEAD:$TRAVIS_BRANCH
+    fi
 else
     bash bin/build/travis/ghpages-build.sh
 fi
--- bin/build/travis/dist-build.sh
+++ bin/build/travis/dist-build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-echo "$TRAVIS_BRANCH and ${TRAVIS_BRANCH:0:1}"
+npm run build
 
-if [ "$TRAVIS_BRANCH" == "develop" ] || "$TRAVIS_BRANCH" == "master" || [ ${TRAVIS_BRANCH:0:1} == "v" ]; then
-    echo "Building distribution for latest push to develop or tag."
-    npm run build
-    mv ./dist "./$TRAVIS_BRANCH"
-    rsync -e 'ssh -i /tmp/cloud_rsa' -r --delete-after --quiet "./$TRAVIS_BRANCH" "milesap@fgpv.org:/disk/static/builds/$TRAVIS_REPO_SLUG"
-    rm -rf "./$TRAVIS_BRANCH"
+# Copy build files into a "plugins" folder which is included with npm publish
+mkdir plugins
+for file in {dist/**/*.js,dist/**/*.css}; do cp "$file" plugins/$(basename "${file}");done
 
-    mv ./docs "./$TRAVIS_BRANCH"
-    rsync -e 'ssh -i /tmp/cloud_rsa' -r --delete-after --quiet "./$TRAVIS_BRANCH" "milesap@fgpv.org:/disk/static/docs/$TRAVIS_REPO_SLUG"
-fi
+mv ./dist "./$TRAVIS_BRANCH"
+rsync -e 'ssh -i /tmp/cloud_rsa' -r --delete-after --quiet "./$TRAVIS_BRANCH" "milesap@fgpv.org:/disk/static/builds/$(basename "$TRAVIS_REPO_SLUG")"
+rm -rf "./$TRAVIS_BRANCH"
+
+mv ./docs "./$TRAVIS_BRANCH"
+rsync -e 'ssh -i /tmp/cloud_rsa' -r --delete-after --quiet "./$TRAVIS_BRANCH" "milesap@fgpv.org:/disk/static/docs/$(basename "$TRAVIS_REPO_SLUG")"
+
+tsc
+npm run sass
