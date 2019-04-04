528f824a57e2c23bcf19445f059402a4c2938e27
Miles Petrov <miles.petrov@canada.ca>
2019-04-04 12:11:48 -0400

    chore(build): update npm publish

--- bin/build/travis/before_deploy.sh
+++ bin/build/travis/before_deploy.sh
@@ -3,6 +3,13 @@
 if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/plugins" ]; then
     openssl aes-256-cbc -K $encrypted_acf2fcf52a75_key -iv $encrypted_acf2fcf52a75_iv -in bin/build/cloud_rsa.enc -out /tmp/cloud_rsa -d
     chmod 600 /tmp/cloud_rsa
+
+    if [ ${TRAVIS_BRANCH:0:1} != "v" ]; then
+        echo $TRAVIS_BRANCH
+        npm version prerelease -m "Travis npm release"
+        CURRENT_VERSION=$(node -p "require('./package.json').version")
+        git push "https://$GITHUB_TOKEN@github.com/fgpv-vpgf/plugins.git" "v$CURRENT_VERSION" HEAD:$TRAVIS_BRANCH
+    fi
 else
     bash bin/build/travis/ghpages-build.sh
 fi
