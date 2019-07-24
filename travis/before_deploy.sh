babc1e535c4bbe06af36220ea2ff1ea0a771a905
Johann Levesque <jolevesq@nrcan.gc.ca>
2019-07-24 10:41:35 -0400

    Updates

---
+++ travis/before_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "fgpv-vpgf/plugins" ]; then
+    openssl aes-256-cbc -K $encrypted_acf2fcf52a75_key -iv $encrypted_acf2fcf52a75_iv -in bin/build/cloud_rsa.enc -out /tmp/cloud_rsa -d
+    chmod 600 /tmp/cloud_rsa
+else
+    bash bin/build/travis/ghpages-build.sh
+fi
