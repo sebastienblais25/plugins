3ba8c8093c48b24bf80eab00809026f13d0593f6
Johann Levesque <jolevesq@nrcan.gc.ca>
2019-10-09 10:42:30 -0400

    fix(zoom): Polygon not showing entirely. Clean all the zoom geometry at the same time Closes 273

--- lib/osdp/index.d.ts
+++ lib/osdp/index.d.ts
@@ -3,26 +3,24 @@ export default class OSDP {
         [id: string]: OSDP;
     };
     init(api: any): void;
+    setZoomEndEvent(mapi: any): void;
     addLayerByUUID(uuid: string): void;
     addLayerByConfig(mapId: string, config: any): void;
     layersEvents(myFunction: any): void;
+    testService(layer: any): void;
     setDefinitonQuery(mapId: string, layerId: string, query: string): void;
     resetDefinitionQuery(mapId: string, layerId: string): void;
     initUpdateExtentBox(): void;
     getExtentBox(): any;
-    inputParsePoints(values: string): any;
-    inputParsePolygons(values: string): any;
+    inputParse(values: string, parseType: string): any;
     addPointsGeometry(mapId: string, values: string): void;
     addPolygonsGeometry(mapId: string, values: string): void;
     removeGeometries(mapId: string): void;
-    createExtentGeom(values: any): string;
     zoomPt(mapId: string, value: string): void;
-    zoomPoly(mapId: string, value: string): void;
-    zoomPoints(mapId: string, value: string): void;
+    zoomExtent(mapId: string, coords: [], expand?: number): void;
+    zoomWkt(mapId: string, values: string, type: string): void;
     saveState(mapid: string): void;
     loadState(mapid: string): void;
-    validate(values: string): boolean;
-    reportErrors(errors: any): void;
 }
 export default interface OSDP {
     api: any;
--- osdp/index.ts
+++ osdp/index.ts
@@ -130,25 +130,13 @@ export default class OSDP {
         return this.extentbox;
     }
 
-    inputParsePoints(values:string): any{
-        const arrayPt = values.split(';');
-        arrayPt.forEach((element, index, arr) => {
-            let elt = element;
-            elt = elt.replace('POINT', '').replace(/\( */g, '[').replace(/ *\)/g, ']');
-            elt = elt.trim();
-            elt = elt.replace(/ +/g,', ');
-            arr[index] = elt;
-        });
-        return `[${arrayPt}]`;
-    }
-
-    inputParsePolygons(values:string): any{
+    inputParse(values:string, parseType: string): any{
         const arrayPoly = values.split(';');
         arrayPoly.forEach((element, index, arr) => {
             let elt = element;
-            elt = elt.replace('POLYGON', '').replace(/\( *\( */g,'[[').replace(/ *\) *\)/g, ']]');
+            elt = elt.replace(parseType, '').replace(/\( */g, '[').replace(/ *\)/g, ']');
             elt = elt.trim();
-            elt = elt.replace(/, */g, '],[');
+            elt = elt.replace(/, */g, '],['); // specific for polygons
             elt = elt.replace(/ +/g,', ');
             arr[index] = elt;
         });
@@ -156,41 +144,44 @@ export default class OSDP {
     }
 
     addPointsGeometry(mapId: string, values: string): void {
-        const input = this.inputParsePoints(values);
         const myMap = (<any>window).RAMP.mapById(mapId);
+        const input = this.inputParse(values, 'POINT');
         const icon = 'M18 8c0-3.31-2.69-6-6-6S6 4.69 6 8c0 4.5 6 11 6 11s6-6.5 6-11zm-8 0c0-1.1.9-2 2-2s2 .9 2 2-.89 2-2 2c-1.1 0-2-.9-2-2zM5 20v2h14v-2H5z';
         const ptcords = JSON.parse(input);
 
         const graphicsOSDP = myMap.layers.getLayersById('graphicsOSDP')[0];
-        const points = [];
         for (let value of ptcords) {
             // create a point with unique id, we'll use an svg path for the icon
-            const x = value[0];
-            const y = value[1];
-            let pt = new (<any>window).RAMP.GEO.Point(`location${Math.round(Math.random() * 100000)}`, [x, y], { style: 'ICON', icon: icon, colour: [255, 0, 0], width: 30 });
-            // add the point to the simple layer
+            let pt = new (<any>window).RAMP.GEO.Point(`location${Math.round(Math.random() * 100000)}`, [value[0], value[1]],
+                { style: 'ICON', icon: icon, colour: [255, 0, 0], width: 30 });
+
+            // add the point to the graphic layer
             graphicsOSDP.addGeometry(pt);
-            points.push(pt)
         }
 
-        // get the extent geometry from multi-point
-        if (points.length === 1) {
+        // zoom to extent of point(s)
+        if (ptcords.length === 1) {
             this.zoomPt(mapId, values);
         } else {
-            const extent = this.createExtentGeom(points);
-            this.zoomPoints(mapId, extent);
+            this.zoomExtent(mapId, ptcords);
         }
     }
 
     addPolygonsGeometry(mapId: string, values: string): void {
-        const input = this.inputParsePolygons(values);
-        const poly1 = new (<any>window).RAMP.GEO.Polygon(0, JSON.parse(input));
-        const polyAll = new (<any>window).RAMP.GEO.MultiPolygon(`location${Math.round(Math.random() * 100000)}`, [poly1], { outlineColor: [255, 0, 0], outlineWidth: 3 });
         const myMap = (<any>window).RAMP.mapById(mapId);
+        const input = this.inputParse(values, 'POLYGON');
+        const poly1 = new (<any>window).RAMP.GEO.Polygon(0, JSON.parse(input));
 
+        // create a multipolygon with unique id
+        const polyAll = new (<any>window).RAMP.GEO.MultiPolygon(`location${Math.round(Math.random() * 100000)}`, [poly1],
+            { outlineColor: [255, 0, 0], outlineWidth: 3 });
+
+        // add the multipolygon to the graphic layer
         const graphicsOSDP = myMap.layers.getLayersById('graphicsOSDP')[0];
         graphicsOSDP.addGeometry([polyAll]);
-        this.zoomPoly(mapId, values);
+
+        // zoom to extent of polygon(s)
+        this.zoomExtent(mapId, JSON.parse(input)[0], 1.25);
     }
 
     removeGeometries(mapId: string) {
@@ -199,43 +190,10 @@ export default class OSDP {
         graphicsOSDP.removeGeometry();
     }
 
-    createExtentGeom(values: any) {
-        let xMin = -999;
-        let yMin = -999;
-        let xMax = -999;
-        let yMax = -999;
-
-        let arraylist = values;
-        if (values.type === 'Polygon') {
-            arraylist = values.ringArray;
-        }
-
-        for (let value of arraylist) {
-            let x = value._xy.x;
-            let y = value._xy.y;
-
-            if (x < xMin || xMin === -999) {
-                xMin = x;
-            }
-            if (y < yMin || yMin === -999) {
-                yMin = y;
-            }
-            if (x > xMax || xMax === -999) {
-                xMax = x;
-            }
-            if (y > yMax || yMax === -999) {
-                yMax = y;
-            }
-        }
-
-        // we already have function to zoom to WKT so create the extent in this format
-        return `POLYGON((${xMin} ${yMin},${xMin} ${yMax},${xMax} ${yMax},${xMax} ${yMin},${xMin} ${yMin}))`
-    }
-
     zoomPt(mapId: string, value: string): void {
         const myMap = (<any>window).RAMP.mapById(mapId);
         const ramp = (<any>window).RAMP;
-        const input = this.inputParsePoints(value);
+        const input = this.inputParse(value, 'POINT');
         const ptcoords = JSON.parse(input);
         const pt = new ramp.GEO.XY(parseFloat(ptcoords[0][0]), parseFloat(ptcoords[0][1]));
 
@@ -243,68 +201,29 @@ export default class OSDP {
         myMap.setCenter(pt);
     }
 
-    zoomPoly(mapId: string, value: string): void {
-        const input = this.inputParsePolygons(value);
-        const poly1 = new (<any>window).RAMP.GEO.Polygon(201, JSON.parse(input));
-        const polyAll = new (<any>window).RAMP.GEO.MultiPolygon(206, [poly1]);
+    zoomExtent (mapId: string, coords: [], expand: number = 1): void {
         const myMap = (<any>window).RAMP.mapById(mapId);
         const ramp = (<any>window).RAMP;
-        const polyarray =  polyAll.polygonArray;
-        const ext_x = [];
-        const ext_y = [];
-        const coords = polyarray[0].ringArray[0].pointArray;
-        coords.forEach((item: any) => {
-            ext_x.push(item._xy.x);
-            ext_y.push(item._xy.y);
-        });
 
-        // project values
-        const extentbox = [[Math.min(...ext_x), Math.min(...ext_y)], [Math.min(...ext_x), Math.max(...ext_y)],
-        [Math.max(...ext_x), Math.max(...ext_y)], [Math.max(...ext_x), Math.min(...ext_y)],
-        [Math.min(...ext_x), Math.min(...ext_y)]];
+        let x = [];
+        let y = [];
+        coords.forEach(item => {
+            x.push(item[0]);
+            y.push(item[1]);
+        })
 
-        const poly = ramp.GAPI.proj.localProjectGeometry('EPSG:3978', {
-            'rings': [extentbox], 'spatialReference': { 'wkid': 4326 }
-        });
+        let ext = ramp.GAPI.proj.projectEsriExtent({
+            'xmin': Math.min(...x), 'ymin': Math.min(...y), 'xmax': Math.max(...x), 'ymax': Math.max(...y),
+            'spatialReference': { 'wkid': 4326 } }, myMap.esriMap.spatialReference);
 
-        const xarray = [];
-        const yarray = [];
-        poly.rings[0].forEach((item: any) => { xarray.push(item[0]); yarray.push(item[1]); });
-
-        myMap.setExtent({
-            'xmin': Math.min(...xarray), 'ymin': Math.min(...yarray), 'xmax': Math.max(...xarray), 'ymax': Math.max(...yarray),
-            'spatialReference': { 'wkid': 3978 }
-        });
-
+        myMap.setExtent(ext.expand(expand));
     }
 
-    zoomPoints(mapId: string, value: string): void {
-        const myMap = (<any>window).RAMP.mapById(mapId);
-        const ramp = (<any>window).RAMP;
-        const split = value.substring(value.indexOf('((') + 2, value.length - 2).split(',');
+    zoomWkt(mapId: string, values: string, type: string) {
+        const input = this.inputParse(values, type);
 
-        // for each coord, create a pair and add it to the coords array
-        const coords = [];
-        split.forEach((item) => {
-            const pair = item.split(' ');
-            coords.push([parseFloat(pair[0]), parseFloat(pair[1])]);
-        });
-
-        // project values
-        const poly = ramp.GAPI.proj.localProjectGeometry('EPSG:3978', {
-            'rings': [coords], 'spatialReference': { 'wkid': 4326 }
-        });
-
-        // get array of x and y
-        const xarray = [];
-        const yarray = [];
-        poly.rings[0].forEach((item: any) => { xarray.push(item[0]); yarray.push(item[1]); });
-
-        // set extent
-        myMap.setExtent({
-            'xmin': Math.min(...xarray), 'ymin': Math.min(...yarray), 'xmax': Math.max(...xarray), 'ymax': Math.max(...yarray),
-            'spatialReference': { 'wkid': 3978 }
-        });
+        // zoom to extent of wkt points or polygon(s)
+        this.zoomExtent(mapId, JSON.parse(input)[0]);
     }
 
     saveState(mapid: string): void {
